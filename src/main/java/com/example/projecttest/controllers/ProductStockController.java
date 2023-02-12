1f278ca41a7c54c87b9eb8951385559083edf5ee
amuazm <amuaz.mohd@gmail.com>
2023-02-12 15:38:11 +0800

    productstock

---
+++ src/main/java/com/example/projecttest/controllers/ProductStockController.java
@@ -0,0 +1,103 @@
+package com.example.projecttest.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class ProductStockController {
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Button btnHistoryAdd;
+
+    @FXML
+    private TextField btnHistoryCount;
+
+    @FXML
+    private Button btnHistoryRemove;
+
+    @FXML
+    private Button btnLess;
+
+    @FXML
+    private Button btnLessEqual;
+
+    @FXML
+    private Button btnNumber;
+
+    @FXML
+    private Button btnOrderThisItem;
+
+    @FXML
+    private Button btnPercentage;
+
+    @FXML
+    private ChoiceBox<?> cbHistoryInventory;
+
+    @FXML
+    private ChoiceBox<?> cbHistoryType;
+
+    @FXML
+    private Label lblAvailableMax;
+
+    @FXML
+    private Label lblAvailableMaxPercentage;
+
+    @FXML
+    private Label lblInventoryName;
+
+    @FXML
+    private Label lblOrderRule;
+
+    @FXML
+    private Label lblOrderRuleAlt;
+
+    @FXML
+    private Label lblProductName;
+
+    @FXML
+    private ListView<?> lvHistory;
+
+    @FXML
+    private TextField tfHistoryDate;
+
+    @FXML
+    private TextField tfOrderRule;
+
+    @FXML
+    void onBackClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLessClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLessEqualClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onNumberClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onOrderThisItemClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onPercentageClicked(ActionEvent event) {
+
+    }
+
+}
