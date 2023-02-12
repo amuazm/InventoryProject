2369128e3a3f381f5523f9fa6ca64453b7b421a3
amuazm <amuaz.mohd@gmail.com>
2023-02-12 15:54:28 +0800

    productorder

---
+++ src/main/java/com/example/projecttest/controllers/ProductOrderController.java
@@ -0,0 +1,96 @@
+package com.example.projecttest.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class ProductOrderController {
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnGoOrder;
+
+    @FXML
+    private Button btnMarkAsDone;
+
+    @FXML
+    private Button btnPercentage;
+
+    @FXML
+    private Button btnSubmit;
+
+    @FXML
+    private Label lblAfter;
+
+    @FXML
+    private Label lblAfterP;
+
+    @FXML
+    private Label lblBefore;
+
+    @FXML
+    private Label lblBeforeP;
+
+    @FXML
+    private Label lblChange;
+
+    @FXML
+    private Label lblChangeP;
+
+    @FXML
+    private Label lblInventoryName;
+
+    @FXML
+    private Label lblProductName;
+
+    @FXML
+    private RadioButton rdStockUntil;
+
+    @FXML
+    private RadioButton rdStockUp;
+
+    @FXML
+    private TextField tfETA;
+
+    @FXML
+    private TextField tfStockValue;
+
+    @FXML
+    void onCancelClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeleteClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onGoOrderClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMarkAsDoneClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onPercentageClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSubmitClicked(ActionEvent event) {
+
+    }
+
+}
